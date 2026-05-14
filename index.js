ed6c3abc3bf94bbf7e854a32115bfc51e5f0866a
gustavo <smarttokenworld.com>
2026-05-14 06:55:04 -0600

    increase payload limit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-
+app.use(express.jason({limit:'50mb'}));
+app.use(express.ulercoded({limit:'50mb'}));
 app.post('/chat', async (req, res) => {
   try {
     const response = await fetch('https://api.anthropic.com/v1/messages', {
